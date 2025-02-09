0594246df2b4a815b36e731bb37f59ae88d4fc81
rpakulski-cs <bla@bla.pl>
2025-02-09 14:22:19 +0100

    car list added to view

--- DotNetMeritoApp/Models/CarViewModel.cs
+++ DotNetMeritoApp/Models/CarViewModel.cs
@@ -3,4 +3,10 @@ namespace DotNetMeritoApp.Models;
 public class CarViewModel
 {
     public string Name { get; set; }
+
+    public string Type { get; set; }
+
+    public string Color { get; set; }
+
+    
 }
